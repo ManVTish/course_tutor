5b0b03a087bf46dfe913d60acc74437f6b287026
Manish Roy <manishroy0112@gmail.com>
2023-04-25 23:59:42 +0530

    refactored params for create action

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,27 +1,15 @@
 class CoursesController < ApplicationController
+  def index
+    @course_tutor = Tutor.all.includes(:course)
+  end
+
   def create
     new_course = Course.new(
-      course_name: course_params[:course_name],
-      skill: course_params[:skill],
-      fee: course_params[:fee]
+      course_params
     )
     if new_course.save
-      tutor_params = course_params[:tutor_details]
-      new_tutor = []
-      tutor_params.each do |t|
-        new_tutor << Tutor.create(
-          tutor_name: t[:tutor_name],
-          expertise: t[:expertise],
-          degree: t[:degree],
-          yoe: t[:yoe],
-          course_id: new_course.id
-        )
-      end
-      res = {
-        course: new_course,
-        tutor: new_tutor
-      }
-      render json: {res: res, msg: "Course created successful"}
+      build_tutor_details(new_course.id)
+      render json: {msg: "Course created successful"}
     else
       render json: {msg: "#{new_course.errors.full_messages}"}
     end
@@ -29,9 +17,30 @@ class CoursesController < ApplicationController
 
   private
 
-  def course_params
+  def course_tutor_params
     params.require(:course)
           .permit(:course_name, :skill, :fee,
                   tutor_details: [:tutor_name, :expertise, :degree, :yoe])
+  end
+
+  def course_params
+    {
+      course_name: course_tutor_params[:course_name],
+      skill: course_tutor_params[:skill],
+      fee: course_tutor_params[:fee]
+    }
+  end
+
+  def build_tutor_details(course_id)
+    tutor_params = course_tutor_params[:tutor_details]
+    tutor_params.each do |t|
+      Tutor.create(
+        tutor_name: t[:tutor_name],
+        expertise: t[:expertise],
+        degree: t[:degree],
+        yoe: t[:yoe],
+        course_id: course_id
+      )
+    end
   end
 end
