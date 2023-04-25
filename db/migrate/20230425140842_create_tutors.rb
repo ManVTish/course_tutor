c2a4fcd6a3f794d5a351786c3f0c15630fffa1a3
Manish Roy <manishroy0112@gmail.com>
2023-04-25 19:44:07 +0530

    tutor model created & course-tutor related

---
+++ db/migrate/20230425140842_create_tutors.rb
@@ -0,0 +1,15 @@
+class CreateTutors < ActiveRecord::Migration[7.0]
+  def change
+    create_table :tutors do |t|
+      t.string :tutor_name
+      t.string :expertise
+      t.string :degree
+      t.integer :yoe
+      t.references :course, null: false
+
+      t.timestamps
+    end
+
+    add_foreign_key :tutors, :course, column: :course_id
+  end
+end
