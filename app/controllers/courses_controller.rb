ec883142f164bc1c4156efabd323105810ab0055
Manish Roy <manishroy0112@gmail.com>
2023-04-25 20:02:02 +0530

    create method for course

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,4 +1,20 @@
 class CoursesController < ApplicationController
   def create
+    new_course = Course.new(
+      course_params
+    )
+    if new_course.save
+      render json: {res: new_course, msg: "Course created successful"}
+    else
+      render json: {msg: "#{new_course.errors.full_messages}"}
+    end
+  end
+
+  private
+
+  def course_params
+    params.require(:courses)
+          .permit(:course_name, :skill, :fee,
+                  tutorial_details: [])
   end
 end
